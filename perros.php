ec9c3173c4465e5299ae5f9fecf62174417c2743
Alejandro Bermúdez Alba <Alejandrobermudezalba@gmail.com>
2023-03-29 13:42:58 +0200

    subida

--- perros.php
+++ perros.php
@@ -1,28 +1,22 @@
 <?php
-// Hacemos la petición a la API
+
 $url = "https://dog.ceo/api/breeds/list/all";
 $response = json_decode(file_get_contents($url), true);
 
-// Inicializamos el array asociativo
-$breeds = array();
-// Recorremos el resultado y agregamos las razas y sus URLs de imagen al array
-foreach ($response['message'] as $breed => $subBreeds) {
-    if (count($subBreeds) > 0) {
-        foreach ($subBreeds as $subBreed) {
-            $breedName = $subBreed . ' ' . $breed;
-            $imageUrl = "https://dog.ceo/api/breed/" . $breed . "/" . $subBreed . "/images/random";
-            $breeds[$breedName] = $imageUrl;
+
+foreach ($response['message'] as $raza => $subrazas) {
+    if (count($subrazas) > 0) {
+        foreach ($subrazas as $subraza) {
+            $nombreraza = $subraza . ' ' . $raza;
+            $urlimagen = "https://dog.ceo/api/breed/" . $raza . "/" . $subraza . "/images/random";
+            echo $nombreraza . " su URL es " . $urlimagen;
+            echo "<br>";
         }
     } else {
-        $imageUrl = "https://dog.ceo/api/breed/" . $breed . "/images/random";
-        $breeds[$breed] = $imageUrl;
+        $urlimagen = "https://dog.ceo/api/breed/" . $raza . "/images/random";
+        echo $raza . " su URL es " . $urlimagen;
+        echo "<br>";
     }
 }
-$keys=array_keys($breeds);
-foreach($keys as $key){
-    echo $key;
-    echo "<br>";
-}
-// Imprimimos el resultado
-print_r($keys);
+
 ?>
